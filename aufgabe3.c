f1769b2f726cb61d82aed43a63849cb76761d530
Akif Aydin <akifnidya@hotmail.de>
2021-06-18 20:11:47 +0200

    Update aufgabe3.c
    finished better code

--- aufgabe3.c
+++ aufgabe3.c
@@ -1,46 +1,89 @@
+#define NUMBER_OF_NAMES 10  //Konstante für die Anzahl der Adressanten
+#define NUMBER_OF_SIZE 100 //Größe der Adressanten
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    FILE* fp;
-    int errno = 0;
-    int mz = 7;
-    char in1[10], in2[10], in3[10];
-
-    // fopen mit "w" erzeugt leere Datei, wenn sie schon existiert
-    // wird sie geloescht und neu erzeugt
-    fp = fopen("27-5-1.txt", "w");
-    if (fp == 0) {
+    
+    FILE* fp;  // pointer
+   
+    char einladung[100] = {0}; //ganze Einladung
+    char* firstP;        //Teil 1 der Einladung
+    char* secondP;       //Teil 2 der Einladung
+    char namen[NUMBER_OF_SIZE] = {0};     //String mit Namen der Adressanten
+    char* split;         //String für die einzelnen Namen in der Schleife
+    int counter = 0;     //Zähler für die Schleife zum Finden der verschiedenen Namen
+    char Adressanten[NUMBER_OF_NAMES][NUMBER_OF_SIZE] = {{0}};  //Anzahl Adressanten und wie groß diese sein dürfen
+    char filename[20] = {0};   //Variable für Dateinamen der Briefe
+    
+    fp = fopen("Die Adressanten.txt", "w"); //Erstellen der Adressanten Datei falls nicht vorhanden
+    // nicht erfolgreich keine Speicheradresse
+    if (NULL == fp) {
         printf("sch... passiert\n");
-        errno = 1;
-        return errno; //errno 1
+        return -1; //fehler passiert
     }
-    for (mz = 0;mz < 10;mz++) {
-        // fprintf schreibt in eine Datei,
-        // sie muss vorher zum Schreiben geoeffnet sein
-        // der Formatstring beschreibt wie geschrieben wird
-        fprintf(fp, "Meine Zahl ist %d\n\r", mz);
-    }
-    fclose(fp);
-    // fopen mit "r" oeffnet Datei zum Lesen,
-    // sie muss schon existieren
-    fp = fopen("27-5-1.txt", "r");
-    if (fp == 0) {
+    fprintf(fp, "Sara, Anton, Maximilian, Erwin, Erna"); //schreibt die Adressanten in die Datei
+    fclose(fp); //Datei Schließen
+    
+    fp = fopen("Der Text.txt", "w"); //Erstellen der Text.txt Datei falls nicht vorhanden
+    // nicht erfolgreich keine Speicheradresse
+    if (NULL == fp) {
         printf("sch... passiert\n");
-        errno = 1; //errno 2
+        return -1; //fehler passiert
     }
-    else
-    {
-        printf("Erfolg\n");
-        // fscanf sliest aus einer Datei, sie muss vorher
-        // zum Lesen geoeffnet sein
-        // der Formatstring beschreibt wie geschrieben wird
-
-        fscanf(fp, "%s %s %s %d", in1, in2, in3, &mz);
-        fscanf(fp, "%s %s %s %d", in1, in2, in3, &mz);
-        fscanf(fp, "%s %s %s %d", in1, in2, in3, &mz);
-        fscanf(fp, "%s %s %s %d", in1, in2, in3, &mz);
-    }
+    fprintf(fp, "Liebe(r) ###, du bist herzlich zu meiner Semesterende Party eingeladen. Dein Wilhelm"); //Schreibt unbefüllte Einladung
     fclose(fp);
-    //printf("Hello, World!\n");
-    return errno;
+
+    fp = fopen("Der Text.txt", "r"); //Lesen der Text.txt Datei mit "r"
+    // nicht erfolgreich keine Speicheradresse
+    if (NULL == fp) {
+        printf("sch... passiert\n");
+        return -1; //fehler passiert
+    }
+    fgets(einladung, NUMBER_OF_SIZE, fp); //Die Einladung von der Datei wo der Pointer fp hin zeigt in firstP speichern
+
+    firstP = strtok(einladung, "#"); //den String an der Stelle find(###) aufteilen und den erster Teil in firstP
+    //puts(firstP);
+
+    secondP = strtok(NULL, "#"); // zweiter Teil schaut mit Null auf den letzten Teil des Strings, wird in secondP initialisiert
+    //puts(secondP);
+    fclose(fp);  //Datei Schließen
+   
+    fp = fopen("Die Adressanten.txt", "r"); //Lesen der  Adressanten.txt Datei mit "r"
+    // nicht erfolgreich keine Speicheradresse
+    if (NULL == fp) {
+        printf("sch... passiert\n");
+        return -1; //fehler passiert
+    }
+    fgets(namen, NUMBER_OF_SIZE, fp);
+    split = strtok(namen, ","); //ersten Namen in split speichern
+    while (split != NULL) {     //Schleife solange durchführen, bis kein Name mehr gefunden wird
+        strcpy(Adressanten[counter], split); //Namen in Array eintragen
+        //printf("%s\n", split);
+        split = strtok(NULL, ", "); //weitere Namen für jeden Durchlauf der Schleife in split speichern
+        counter++;
+    }
+
+    for (int i = 0; i < counter; i++) { //for-Schleife 0 bis Anzahl Adressanten
+
+        sprintf(filename, "%sBrf.txt", Adressanten[i]); //namen der Datei in filename initialisieren z.B SaraBrf.txt
+        fp = fopen(filename, "w");  //Datei erstellen mit dem Namen in filename Bsp. SaraBrf.txt
+        // nicht erfolgreich keine Speicheradresse
+        if (NULL == fp) {
+            printf("sch... passiert\n");
+            return -1; //fehler passiert
+        }
+
+        char newEinladung[strlen(firstP) + strlen(Adressanten[i]) + strlen(secondP) + 1];
+        strcpy(newEinladung, firstP);  //neuen Einladungstext mit Namen zusammenfügen (53-55)
+        strcat(newEinladung, Adressanten[i]);
+        strcat(newEinladung, secondP);
+        fprintf(fp, "%s", newEinladung); //neuen Einladungstext in die entsprechende Datei eintragen
+
+            fclose(fp); //schließt die Date auf welche der Pointer fp zeigt
+    }
+
+
+
+    return 0;
 }
